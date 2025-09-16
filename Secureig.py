389b2ee66dc49b2e2679882794076a84461ff7bc
tslime <taha.slime@gmail.com>
2025-09-16 09:41:13 -0400

    http server implementation completed

--- Secureig.py
+++ Secureig.py
@@ -1,7 +1,19 @@
 import os
 import sys
+import uvicorn
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 
+connection_point = FastAPI()
+
+def launch_http_server(connection_point,ip,p):    
+    uvicorn.run(connection_point,host=ip,port=p)
+
+@connection_point.post("/request")
+def send_response():
+    return {"message": "Connection, OK!"}
 
 
-print("test \n")
+launch_http_server(connection_point,"192.168.2.57",8085)
